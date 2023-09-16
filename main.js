7fc6151043016c264a7abf601ab72589545ce9b8
Lewis-Underhill <35866051+Lewis-Underhill@users.noreply.github.com>
2023-09-16 16:54:07 +0100

    generateName function added to create a full character name

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ const arrayImport = textFile => {
     return items;
 }
 
+// Function to return a random item from the input array
+const rand = array => {
+    let index = Math.floor(Math.random() * array.length);
+    return array[index];
+}
+
 // Arrays of character name attributes
 const mTitles = arrayImport('arrays/maletitles.txt');
 const fTitles = arrayImport('arrays/femaletitles.txt');
@@ -15,17 +21,78 @@ const surnames = arrayImport('arrays/surnames.txt');
 const accolades = arrayImport('arrays/accolades.txt');
 const places = arrayImport('arrays/places.txt');
 
-// Function to return a random item from the input array
-const rand = array => {
-    let index = Math.floor(Math.random() * array.length);
-    return array[index];
+// Generate name (and title)
+const generateName = () => {
+    
+    // Choose 1 of 2 options at random
+    const binaryChoice = (option1, option2) => {
+        if (Math.random() < 0.5) {
+            return option1;
+        } else {
+            return option2;
+        }
+    }
+
+    // Determine character gender
+    const gender = binaryChoice('male', 'female');
+
+    // Determine whether to add a title
+    const title = binaryChoice(true, false);
+
+    // Determine number of names before last name
+    const nameNumber = () => {
+        let d100 = Math.floor(Math.random() * 100);
+
+        if (d100 < 65) {
+            return 1;
+        } else if (d100 < 90) {
+            return 2;
+        } else if (d100 < 92) {
+            return 3;
+        } else if (d100 < 94) {
+            return 4;
+        } else if (d100 < 96) {
+            return 5;
+        } else if (d100 < 97) {
+            return 6;
+        } else if (d100 < 98) {
+            return 7;
+        } else if (d100 < 99) {
+            return 8;
+        } else if (d100 === 99) {
+            return 9;
+        }                
+    }
+
+    // Choose first name(s) and title if relevant
+    let name1 = '';
+    
+    if (gender === 'male') {
+
+        if (title) {
+            name1 += rand(mTitles) + ' ';
+        }
+
+        for (let i = 0; i < nameNumber(); i++) {
+            name1 += rand(mFirstNames) + ' ';
+        }
+
+    } else {
+
+        if (title) {
+            name1 += rand(fTitles) + ' ';
+        }
+
+        for (let i = 0; i < nameNumber(); i++) {
+            name1 += rand(fFirstNames) + ' ';
+        }
+    }
+
+    //Choose surname
+    let name2 = rand(surnames);
+
+    return name1 + name2;
 }
 
 
-console.log(rand(mTitles));
-console.log(rand(fTitles));
-console.log(rand(mFirstNames));
-console.log(rand(fFirstNames));
-console.log(rand(surnames));
-console.log(rand(accolades));
-console.log(rand(places));
+console.log(generateName());
