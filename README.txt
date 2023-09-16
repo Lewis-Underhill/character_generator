b82a6b98611c519aef88a91d0a523d36a947ba61
Lewis-Underhill <35866051+Lewis-Underhill@users.noreply.github.com>
2023-09-16 13:55:42 +0100

    Function added to return a random item an array

--- README.txt
+++ README.txt
@@ -1,3 +1,4 @@
 Codecademy Portfolio Project - Mixed Messages:
 
-A message generator program using JavaScript
+A message generator program using JavaScript - randomly generated message contains a name, 
+accolade and place of origin for a fictional character. 
--- main.js
+++ main.js
@@ -6,5 +6,26 @@ const arrayImport = textFile => {
     return items;
 }
 
-const test = arrayImport('arrays/test.txt');
-console.log(test);
+// Arrays of character name attributes
+const mTitles = arrayImport('arrays/maletitles.txt');
+const fTitles = arrayImport('arrays/femaletitles.txt');
+const mFirstNames = arrayImport('arrays/malefirstnames.txt');
+const fFirstNames = arrayImport('arrays/femalefirstnames.txt');
+const surnames = arrayImport('arrays/surnames.txt');
+const accolades = arrayImport('arrays/accolades.txt');
+const places = arrayImport('arrays/places.txt');
+
+// Function to return a random item from the input array
+const rand = array => {
+    let index = Math.floor(Math.random() * array.length);
+    return array[index];
+}
+
+
+console.log(rand(mTitles));
+console.log(rand(fTitles));
+console.log(rand(mFirstNames));
+console.log(rand(fFirstNames));
+console.log(rand(surnames));
+console.log(rand(accolades));
+console.log(rand(places));
